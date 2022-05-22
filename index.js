d0c9a0bf2c8ebe7555830a4432df3dfaaf317612
MdMuzahid07 <mdmuzahid7396@gmail.com>
2022-05-22 22:35:07 +0600

    working on it

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yof5x.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 const run = async () => {
 
     try {
-        
+
         await client.connect();
 
         const serviceCollection = client.db('cleaningService').collection('service');
@@ -27,8 +27,47 @@ const run = async () => {
 
         console.log("MongoDb connected?");
 
-        app.get('service', async(req, res) => {
+        app.get('/service', async (req, res) => {
+            const services = await serviceCollection.find({}).toArray();
+            res.send(services)
+        })
+
+
+        app.post('/add-service', async (req, res) => {
+            const data = req.body;
+
+            const result = await serviceCollection.insertOne(data);
+
+            res.send(result)
+        })
+
+
+        // to update exists data from database
+
+        app.put('/update-service/:id', async (req, res) => {
+            const { id } = req.params;
+            const data = req.body;
+
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: data
+            }
+
+            const result = await serviceCollection.updateOne(filter, updateDoc, options)
+            res.send(result)
+        })
+
+        // to delete exists data form databsae
+
+        app.delete('/delete-service/:id', async (req, res) => {
+            const { id } = req.params;
+
+            const query = { _id: ObjectId(id) };
             
+
+            const result = await serviceCollection.deleteOne(query)
+            res.send(result)
         })
 
 
@@ -41,7 +80,9 @@ const run = async () => {
 run().catch(console.dir);
 
 
-app.get('/', async(req, res) => {
+
+
+app.get('/', async (req, res) => {
     res.send("Server running?")
 });
 
